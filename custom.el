8a3b91a50b3029e0a390a5c7f9b28cf5757adfa3
Ned Rihine <ned.rihine@gmail.com>
2024-02-08 21:29:48 +0900

    fixed-pitch のフォントを FirgeNerd に

--- custom.el
+++ custom.el
@@ -4,10 +4,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(wren-mode xml+ purescript-mode geiser-guile typescript-mode markdown-mode yaml-mode c-eldoc irony multi-web-mode web-mode web-narrow-mode haskell-mode julia-mode geiser-gambit ob-fsharp ob-hy ob-dart ob-deno ob-kotlin ob-mermaid ob-raku ruby-electric svelte-mode haxe-imports haxe-mode tree-sitter-langs go-mode fsharp-mode mozc-popup mozc meson-mode cmake-ide cmake-project nim-mode creole creole-mode crystal-playground groovy-mode psc-ide quack tree-sitter-indent tree-sitter rjsx-mode emmet-mode csv-mode geiser-chicken geiser-gauche htmlize dart-mode dockerfile-mode quelpa quelpa-use-package tr-ime clojure-mode company raku-mode rust-mode sass-mode scala-mode smalltalk-mode yasnippet json-mode lua-mode nix-mode)))
+   '(ob-elixir ob-elm ob-go ob-haxe ob-ipython ob-lfe ob-rust ob-sql-mode tuareg wren-mode xml+ purescript-mode geiser-guile typescript-mode markdown-mode yaml-mode c-eldoc irony multi-web-mode web-mode web-narrow-mode haskell-mode julia-mode geiser-gambit ob-fsharp ob-hy ob-dart ob-deno ob-kotlin ob-mermaid ob-raku ruby-electric svelte-mode haxe-imports haxe-mode tree-sitter-langs go-mode fsharp-mode mozc-popup mozc meson-mode cmake-ide cmake-project nim-mode creole creole-mode crystal-playground groovy-mode psc-ide quack tree-sitter-indent tree-sitter rjsx-mode emmet-mode csv-mode geiser-chicken geiser-gauche htmlize dart-mode dockerfile-mode quelpa quelpa-use-package tr-ime clojure-mode company raku-mode rust-mode sass-mode scala-mode smalltalk-mode yasnippet json-mode lua-mode nix-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(fixed-pitch ((t (:family "FirgeNerd"))))
+ '(font-lock-constant-face ((t (:foreground "#d55e00" :weight bold :family "FirgeNerd")))))
